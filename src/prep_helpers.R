57e9428a1ab5419bed10e99a9a1baa8e59ee1de2
Platt <lplatt@usgs.gov>
2023-01-27 14:12:23 -0600

    add helper for identifying LSTM site ids

--- src/prep_helpers.R
+++ src/prep_helpers.R
@@ -1,8 +1,16 @@
 
-prep_lake_locations <- function(data_file, lstm_metadata_file, repo_path = '../lake-temperature-model-prep') {
+# Lakes included in LSTM represent the full footprint of what is included in this release
+# The vector returned here is used for filtering other targets
+prep_site_ids <- function(lstm_metadata_file) {
+  read_csv(lstm_metadata_file) %>% 
+    filter(lstm_predictions) %>% 
+    pull(site_id)
+}
+
+prep_lake_locations <- function(data_file, lakes_in_release, repo_path = '../lake-temperature-model-prep') {
   scipiper_freshen_files(data_file)
-  lake_centroids_sf <- readRDS(data_file) # Load lake centroids
-  lstm_lakes <- read_csv(lstm_metadata_file) %>% filter(lstm_predictions) %>% pull(site_id)
-  lake_centroids_sf_lstm <- lake_centroids_sf %>% filter(site_id %in% lstm_lakes) # Filter lake centroids sf object to LSTM lakes only
-  return(lake_centroids_sf_lstm)
+  
+  # Load lake centroids and filter to those included in this release
+  readRDS(data_file) %>% 
+    filter(site_id %in% lakes_in_release) 
 }
