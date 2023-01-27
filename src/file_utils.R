100d0c793509002018c76d963355046fd5f145a7
Platt <lplatt@usgs.gov>
2023-01-27 13:56:11 -0600

    add helper to make sure that the files in upstream scipiper pipelines are up-to-date

--- src/file_utils.R
+++ src/file_utils.R
@@ -1,4 +1,25 @@
 
+# Make sure that the data file from `lake-temperature-model-prep` is downloaded and up-to-date
+scipiper_freshen_files <- function(ind_files = NULL, data_files = NULL, repo_path = '../lake-temperature-model-prep/') {
+  message(sprintf('Temporarily setting working directory to > %s', repo_path))
+  cwd <- getwd()
+  setwd(repo_path)
+  
+  # Prep files
+  if(is.null(ind_files)) ind_files <- sprintf('%s.ind', data_files) 
+  ind_files <- gsub(repo_path, '', ind_files) # Drop repo path since we switched working dirs
+  
+  message('Freshening...')
+  purrr::map(ind_files, function(ind_fn) {
+    message(sprintf('    %s', ind_fn))
+    scipiper::sc_retrieve(ind_fn)
+  })
+  message('All have been freshened.')
+  
+  message(sprintf('Resetting working directory to > %s', cwd))
+  setwd(cwd)
+  return(ind_files)
+} 
 
 sf_to_zip <- function(zip_filename, sf_object, layer_name){
   cdir <- getwd()
