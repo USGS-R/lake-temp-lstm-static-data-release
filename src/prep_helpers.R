44279686fa122ed13888beeef4641c155b5ec3af
Platt <lplatt@usgs.gov>
2023-01-27 14:18:29 -0600

    add target for lake_hypsography.csv

--- src/prep_helpers.R
+++ src/prep_helpers.R
@@ -14,3 +14,21 @@ prep_lake_locations <- function(data_file, lakes_in_release, repo_path = '../lak
   readRDS(data_file) %>% 
     filter(site_id %in% lakes_in_release) 
 }
+
+prep_lake_hypsography <- function(out_file, data_file, lakes_in_release, repo_path = '../lake-temperature-model-prep') {
+  scipiper_freshen_files(data_file)
+  
+  # Load the full list of hypsography available and then  
+  # filter out lakes that are not included in this release
+  H_A_list <- readRDS(data_file)
+  H_A_list_in_release <- H_A_list[names(H_A_list) %in% lakes_in_release]
+  
+  hypso_list <- purrr::map(H_A_list_in_release, function(H_A_df) {
+    H_A_df %>% 
+      mutate(depths = max(H) - H, areas = A) %>% 
+      arrange(depths) %>% 
+      select(depths, areas)
+  })
+  hypso_df <- purrr::map_df(hypso_list, ~as.data.frame(.x), .id="site_id")
+  write_csv(hypso_df, out_file)
+}
