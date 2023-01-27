c9a80eb6eb27b02cac0e5a2524885715976b7f9b
Platt <lplatt@usgs.gov>
2023-01-27 14:30:13 -0600

    add target for lake_temperature_observations.zip

--- src/prep_helpers.R
+++ src/prep_helpers.R
@@ -32,3 +32,21 @@ prep_lake_hypsography <- function(out_file, data_file, lakes_in_release, repo_pa
   hypso_df <- purrr::map_df(hypso_list, ~as.data.frame(.x), .id="site_id")
   write_csv(hypso_df, out_file)
 }
+
+prep_lake_temp_obs <- function(out_file, data_file, lakes_in_release, earliest_prediction, repo_path = '../lake-temperature-model-prep') {
+  scipiper_freshen_files(data_file)
+  
+  # Load temperature observations and filter to the appropriate lakes and dates
+  temp_obs_all <- arrow::read_feather(data_file) %>% 
+    filter(date >= as.Date(earliest_prediction)) %>% # Filter to the earliest date available
+    filter(site_id %in% lakes_in_release) %>% # Filter to only sites included in the data release
+    select(-source_id)
+  
+  # Save the CSV in a temporary location
+  tmp_space <- tempdir()
+  obs_csv <- file.path(tmp_space, 'lake_temperature_observations.csv')
+  write_csv(temp_obs_all, obs_csv)
+  
+  # Compress the CSV into a single zip file in this directory
+  zip::zip(out_file, files = obs_csv)
+}
